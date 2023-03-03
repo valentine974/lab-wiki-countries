80dbf386c23e5ec86b8e93226d21b349302c1595
Nicole <92593875+ozzleme@users.noreply.github.com>
2023-03-03 17:45:46 +0530

    iteration 2.4

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import NavBar from './components/NavBar.jsx';
 import CountriesList from './components/CountriesList.jsx';
 import CountryDetails from './components/CountryDetails.jsx';
 // import { useState } from 'react'; 
-import { Route, Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import ErrorPage from './components/ErrorPage'
 
 function App() {
@@ -17,17 +17,12 @@ function App() {
       <NavBar />
 
       <div className="container">
-        <div className="row"> 
-
-          
-
+        <div className="row">
+          <CountriesList countryList={Countries} />
           <Routes>
-          <Route path="/" element={<CountriesList countryList={Countries}  />} />
-          <Route path="/:countryId" element={<CountryDetails countryList={Countries}  />} />
-
-          <Route path="/*" element={<ErrorPage />} />
+            <Route path="/:countryId" element={<CountryDetails countryList={Countries} />} />
+            <Route path="/*" element={<ErrorPage />} />
           </Routes>
- 
         </div>
       </div>
     </div>
--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -1,35 +1,42 @@
-import { useParams } from "react-router-dom"; 
+import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 
-function CountryDetails({countryList}) {
+function CountryDetails({ countryList }) {
 
-const [countrySelected, setCountry] = useState({})    
-const {countryId}= useParams() 
-console.log(countryId)
-useEffect(() => {    
-    console.log("useEffect working")                                
-   const selectedCountry =  countryList.find((countryChecked)=>{ 
-    return countryId  === countryChecked.alpha3Code 
-   })
-   setCountry(selectedCountry)   
+  const [countrySelected, setCountry] = useState({})
 
-}, [countryId, countryList]);
+  const { countryId } = useParams()
 
-  
-  return (  
-     <div className="col-7">
-      <h1>{countrySelected.name.common}</h1>
+  useEffect(() => {
+    setCountry(countryList.find(countryChecked => countryChecked.alpha3Code === countryId))
+  }, [countryList, countryId]);
+
+  const selectedCountry = countryList.find(countryChecked => countryChecked.alpha3Code === countryId)
+
+  // useEffect(() => {
+  //   console.log("useEffect working")
+  //   const selectedCountry = countryList.find((countryChecked) => {
+  //     return countryId === countryChecked.alpha3Code
+  //   })
+  //   setCountry(selectedCountry)
+
+  // }, [countryId, countryList]);
+
+
+  return (
+    <div className="col-7">
+      <h1>{selectedCountry.name.common}</h1>
       <table className="table">
         <thead></thead>
         <tbody>
-          <tr> 
-            <td style={{width: '30%'}}>Capital</td>
-            <td>{countrySelected.capital}</td>
+          <tr>
+            <td style={{ width: '30%' }}>Capital</td>
+            <td>{selectedCountry.capital}</td>
           </tr>
           <tr>
             <td>Area</td>
             <td>
-            {countrySelected.area} km
+              {selectedCountry.area} km
               <sup>2</sup>
             </td>
           </tr>
@@ -37,15 +44,15 @@ useEffect(() => {
             <td>Borders</td>
             <td>
               <ul>
-              <li> item test
-              {/* {bordersArray} */}
-              </li> 
+                <li> item test
+                  {/* {bordersArray} */}
+                </li>
               </ul>
             </td>
           </tr>
         </tbody>
       </table>
-    </div> 
+    </div>
   );
 }
 export default CountryDetails;
